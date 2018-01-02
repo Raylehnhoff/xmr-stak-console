518aa62882d2f6754498f5c8d9071b9a8630fe6a
Raylehnhoff <smdaegan@gmail.com>
2018-01-01 21:59:39 -0700

    Add project files.

---
+++ xmr-stak-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xmr_stak_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
