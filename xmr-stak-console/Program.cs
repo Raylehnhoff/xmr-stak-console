64f7bb7bd43be50df73fe0567174ac53bd0f9721
Raylehnhoff <smdaegan@gmail.com>
2018-01-02 10:31:19 -0700

    initial commit

--- xmr-stak-console/Program.cs
+++ xmr-stak-console/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using xmr_stak_sharp;
 
 namespace xmr_stak_console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new RestClient("http://192.168.86.72:10080");
+            var request = new RestRequest("api.json", Method.GET);
+            var response = client.Execute<XmrStakRoot>(request);
+            //Just an example
+            Console.WriteLine(response.Data.hashrate.total[0] * response.Data.hashrate.highest);
+            Console.WriteLine(client.BuildUri(request));
         }
     }
 }
